90ea1f284340d3799394c6c286f6d82a04597413
dmytro-bieliei <dmbieliei@gmail.com>
2023-01-01 18:02:18 +0200

    create DML in document structure

--- sql/DDL.sql
+++ sql/DDL.sql
@@ -27,17 +27,4 @@ ALTER TABLE workers
 ALTER COLUMN salary SET DEFAULT '150';
 
   
-INSERT INTO workers (
-  name,
-  birthday,
-  salary,
-  is_male,
-  email
-)  
-VALUES (
-  'Nikita',
-  '30-06-1990',
-  300,
-  true,
-  'test@test'
-);
+
---
+++ sql/DML.sql
@@ -0,0 +1,14 @@
+INSERT INTO workers (
+  name,
+  birthday,
+  salary,
+  is_male,
+  email
+)  
+VALUES (
+  'Nikita',
+  '30-06-1990',
+  300,
+  true,
+  'test@test'
+);
